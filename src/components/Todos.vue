ffa484ab6d529efb333ccaff9b36195e9c838107
Cory Coward <cory@c2softwaresolutions.com>
2020-06-14 18:30:16 -0500

    Todos and Todo components created

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>My Todo List</h2>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id">
+        <Todo v-bind:todo="todo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo";
+
+export default {
+  name: "Todos",
+  components: {
+    Todo
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
